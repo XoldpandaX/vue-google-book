27c35fc8989c14a0764a88c328cf99c373f17cf2
XoldpandaX <rembo-87@yandex.ru>
2019-12-27 00:13:41 +0700

    change: change route values from constants

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import { ROUTES } from '@/constants';
+
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: ROUTES.HOME.PATH,
+    name: ROUTES.HOME.NAME,
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: ROUTES.SEARCH.PATH,
+    name: ROUTES.SEARCH.NAME,
+    component: About,
   },
 ];
 
-const router = new VueRouter({
-  routes,
-});
+const router = new VueRouter({ routes });
 
 export default router;
