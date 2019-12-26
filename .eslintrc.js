4853338f8cd5533dc2f52bd4ca886212f7789271
XoldpandaX <rembo-87@yandex.ru>
2019-12-27 00:10:47 +0700

    add: redifine some linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,35 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
+    // common
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', 120, 2, { ignoreUrls: true }],
+    'arrow-parens': 0,
+    // vue
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'LIFECYCLE_HOOKS',
+        'data',
+        'watch',
+        'computed',
+        'methods',
+        ['template', 'render'],
+        'renderError',
+      ],
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
