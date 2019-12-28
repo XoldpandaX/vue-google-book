66d6deb5483bc8ea18411c0b095d4d87282efd8d
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 16:07:38 +0700

    add: add new component for displaying icon as a button

---
+++ src/components/common/button-icon/button-icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-button
+    type="with-icon"
+    @button-click="$emit('button-click')"
+  >
+    <app-icon
+      :name="iconName"
+      :size="size"
+      :color="iconColor || undefined"
+    />
+  </app-button>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+import { AppButton } from '@/components/common/app-button';
+import { AppIcon } from '@/components/common/app-icon';
+
+export default {
+  name: 'button-icon',
+  components: {
+    AppButton,
+    AppIcon,
+  },
+  props: {
+    isEnable: VueTypes.bool.def(true),
+    iconName: VueTypes.string.isRequired,
+    size: VueTypes.shape({
+      width: VueTypes.number,
+      height: VueTypes.number,
+    }),
+    iconColor: VueTypes.string,
+  },
+};
+</script>
