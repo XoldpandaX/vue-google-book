14abedb3bec9d23d83f679c4e3469bf98e8fa31c
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 16:16:11 +0700

    add: add new route structure for  not common components which are uses only in one page

---
+++ src/components/routes/home/form-auth/form-auth.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+    <input type="text" />
+    <button-with-loader @button-click="c">
+      Sign Up
+    </button-with-loader>
+    <button-with-loader
+      :is-enable="false"
+      @button-click="c"
+    >
+      Sign In
+    </button-with-loader>
+  </form>
+</template>
+
+<script>
+import { ButtonWithLoader } from '@/components/common/button-with-loader';
+
+export default {
+  name: 'form-auth',
+  components: {
+    ButtonWithLoader,
+  },
+  methods: {
+    c() {
+      console.info('clicked');
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'form-auth';
+</style>
