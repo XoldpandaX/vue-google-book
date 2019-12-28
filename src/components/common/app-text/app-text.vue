1aecbcb649d5594f3c078a359572ddda06353a5b
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 15:53:15 +0700

    create: add new component for text inline o paragraph

---
+++ src/components/common/app-text/app-text.vue
@@ -0,0 +1,55 @@
+<template>
+  <p
+    v-if="!isInline"
+    :class="rootClasses"
+  >
+    <slot />
+  </p>
+  <span
+    v-else
+    :class="rootClasses"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'app-text',
+  props: {
+    sizeType: VueTypes.oneOf(['primary', 'secondary', 'tertiary']).isRequired,
+    colorType: VueTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary'])
+      .def('primary'),
+    lineHeightType: VueTypes.oneOf(['primary', 'secondary']).def('primary'),
+    isInline: VueTypes.bool.def(false),
+  },
+  computed: {
+    rootClasses() {
+      return [
+        this.baseClass,
+        this.fontSizeClass,
+        this.fontColorClass,
+        this.lineHeightClass,
+      ];
+    },
+    baseClass() {
+      return 'app-text';
+    },
+    fontSizeClass() {
+      return `${this.baseClass}--size-${this.sizeType}`;
+    },
+    fontColorClass() {
+      return `${this.baseClass}--color-${this.colorType}`;
+    },
+    lineHeightClass() {
+      return `${this.baseClass}--line-height-${this.lineHeightType}`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'app-text';
+</style>
