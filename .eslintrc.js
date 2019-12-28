b85eb1328aaabcb79e110a3be70515eb720be27e
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 12:22:33 +0700

    add: new rules for no errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   ],
   rules: {
     // common
+    'template-curly-spacing': 'off',
+    indent: ['error', 2, { ignoredNodes: ['TemplateLiteral'] }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/prefer-default-export': 'off',
