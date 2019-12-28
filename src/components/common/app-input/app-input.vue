0fff994aa00655a86af6f0bba653be43ce9f4e58
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 16:16:36 +0700

    create: create base input component

---
+++ src/components/common/app-input/app-input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    :class="rootClasses"
+    :type="type"
+    :placeholder="placeholder"
+    @input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'app-input',
+  props: {
+    classNames: VueTypes.arrayOf(VueTypes.string).def(['']),
+    type: VueTypes.string.def('text'),
+    placeholder: VueTypes.string.def(''),
+  },
+  computed: {
+    rootClasses() {
+      return ['app-input', ...this.classNames];
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'app-input';
+</style>
