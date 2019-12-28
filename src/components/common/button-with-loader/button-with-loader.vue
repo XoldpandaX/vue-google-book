acc7ec895c6cbe8eaa6dc96817e3869e2977af24
XoldpandaX <rembo-87@yandex.ru>
2019-12-28 16:09:00 +0700

    add: add new button component for displaying button with loading status

---
+++ src/components/common/button-with-loader/button-with-loader.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-button
+    type="primary"
+    size="small"
+    :isEnable="isEnable"
+    @button-click="$emit('button-click')"
+  >
+    <slot v-if="isEnable" />
+    <loader-pulse
+      v-else
+      size="10px"
+      color="#fff"
+    />
+  </app-button>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+import { AppButton } from '@/components/common/app-button';
+import { LoaderPulse } from '@/components/common/loader-pulse';
+
+export default {
+  name: 'button-with-loader',
+  components: {
+    AppButton,
+    LoaderPulse,
+  },
+  props: {
+    isEnable: VueTypes.bool.def(true),
+  },
+};
+</script>
---
+++ src/components/common/button-with-loader/index.js
@@ -0,0 +1,1 @@
+export { default as ButtonWithLoader } from './button-with-loader.vue';
