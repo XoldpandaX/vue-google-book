fb145074cc0f9fe7e62e8acaee3550e56d495afe
XoldpandaX <rembo-87@yandex.ru>
2019-12-30 00:32:24 +0700

    create: create new title component

---
+++ src/components/common/app-title/app-title.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: 'app-title',
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+    typeColor: {
+      type: String,
+    },
+    customColor: {
+      type: String,
+      default: '',
+    },
+    customFontSize: {
+      type: Number,
+    },
+  },
+  render(h) {
+    return h(
+      `h${this.level}`,
+      {
+        class: {
+          'app-title': true,
+          'app-title--color-primary': this.typeColor === 'primary',
+          'app-title--color-secondary': this.typeColor === 'secondary',
+          'app-title--color-tertiary': this.typeColor === 'tertiary',
+          'app-title--color-quaternary': this.typeColor === 'quaternary',
+        },
+        style: {
+          color: !this.typeColor && this.customColor ? this.customColor : '',
+          fontSize: this.customFontSize && `${this.customFontSize}px`,
+        },
+      },
+      this.$slots.default,
+    );
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'app-title';
+</style>
---
+++ src/components/common/app-title/index.js
@@ -0,0 +1,1 @@
+export { default as AppTitle } from './app-title.vue';
