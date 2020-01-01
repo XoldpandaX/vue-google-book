47f6e4653e7c2bea7fb0c8848a193eccc09e1f81
XoldpandaX <rembo-87@yandex.ru>
2020-01-01 22:13:32 +0700

    remove: remove inner form config && transfer to upper lvl

--- src/components/routes/home/form-auth/form-auth-consts.js
+++
@@ -1,9 +0,0 @@
-export default {
-  LOG_IN_SWITCH_BTN_ID: '2b',
-  LOG_IN_BTN_TEXT: 'Log In',
-  LOG_IN_FORM_TITLE: 'Welcome Back!',
-
-  SIGN_UP_SWITCH_BTN_ID: '1b',
-  SIGN_UP_BTN_TEXT: 'Sign Up',
-  SIGN_UP_FORM_TITLE: 'Sign Up Please',
-};
