5ef40e07db0f669f922cc1a6d71d537b02af43be
XoldpandaX <rembo-87@yandex.ru>
2020-01-01 22:21:28 +0700

    add/remove: remove excess code && import auth-form container

--- src/components/routes/home/page/home-page.vue
+++ src/components/routes/home/page/home-page.vue
@@ -1,33 +1,16 @@
 <template>
   <section class="home-page">
-<!--    <img-->
-<!--      src="@/assets/png/logo.png"-->
-<!--      alt="Vue logo"-->
-<!--    >-->
-<!--    <app-rating-->
-<!--      :rating="3.8"-->
-<!--    />-->
-    <form-auth />
+    <container-form-auth />
   </section>
 </template>
 
 <script>
-import { FormAuth } from '../form-auth';
+import { ContainerFormAuth } from '../container-form-auth';
 
 export default {
   name: 'home',
   components: {
-    FormAuth,
-  },
-  data() {
-    return {
-      field: '',
-    };
-  },
-  methods: {
-    c() {
-      console.info('hello');
-    },
+    ContainerFormAuth,
   },
 };
 </script>
