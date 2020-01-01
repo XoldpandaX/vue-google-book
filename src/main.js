bd330e3d83443ac82fdb5f60020094c5d1b515b6
XoldpandaX <rembo-87@yandex.ru>
2020-01-01 22:23:39 +0700

    add: init firebase && add quth logic when page reloads

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
+import * as firebase from 'firebase';
 import router from './router';
-import store from './store';
-import { SettingUpVeeValidate } from './utils';
+import createStore from '@/store';
+import { FIREBASE_CONFIG } from './constants';
+import { settingUpVeeValidate } from './plugins';
 
 import { App } from './components/app';
 
-SettingUpVeeValidate();
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+let app;
+const store = createStore();
+settingUpVeeValidate();
+
+firebase.initializeApp(FIREBASE_CONFIG);
+firebase.auth().onAuthStateChanged(async (user) => {
+  // received user and doesn't have user data in the store - (in case of page reloading)
+  if (!app && user) {
+    await store.dispatch('auth/saveUser', user);
+  }
+
+  app = new Vue({
+    router,
+    store,
+    render: h => h(App),
+  }).$mount('#app');
+});
