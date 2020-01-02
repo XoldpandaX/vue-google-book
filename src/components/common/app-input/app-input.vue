0fec948fc8f8e6a2caeaa6d823a7bb3c85473580
XoldpandaX <rembo-87@yandex.ru>
2020-01-03 01:24:39 +0700

    add: add new props && add arrow press handling && bind prop value to input value

--- src/components/common/app-input/app-input.vue
+++ src/components/common/app-input/app-input.vue
@@ -1,9 +1,13 @@
 <template>
   <input
-    :class="rootClasses"
+    class="app-input"
+    :value="value"
     :type="type"
     :placeholder="placeholder"
+    :style="customWidth"
     @input="$emit('input', $event.target.value)"
+    @keydown.down="handleArrowBtnPress"
+    @keydown.up="handleArrowBtnPress"
   >
 </template>
 
@@ -13,13 +17,32 @@ import VueTypes from 'vue-types';
 export default {
   name: 'app-input',
   props: {
-    classNames: VueTypes.arrayOf(VueTypes.string).def(['']),
+    value: VueTypes.string.isRequired,
     type: VueTypes.string.def('text'),
     placeholder: VueTypes.string.def(''),
+    width: VueTypes.string.def(''),
+    customType: VueTypes.oneOf(['search', '']).def(''),
   },
   computed: {
-    rootClasses() {
-      return ['app-input', ...this.classNames];
+    customWidth() {
+      return {
+        width: this.width,
+        maxWidth: '100%',
+      };
+    },
+  },
+  methods: {
+    handleArrowBtnPress(e) {
+      if (this.customType === 'search') {
+        const { code } = e;
+        const eventsByKeyCode = {
+          ArrowDown: 'press-key-down',
+          ArrowUp: 'press-key-up',
+        };
+
+        e.preventDefault();
+        this.$emit(eventsByKeyCode[code], { key: code });
+      }
     },
   },
 };
