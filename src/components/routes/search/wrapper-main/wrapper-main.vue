476e9fb66032c46744d5efe11e6739b8caf6287c
XoldpandaX <rembo-87@yandex.ru>
2020-01-03 01:34:46 +0700

    add: structure component to bind all search elements together

---
+++ src/components/routes/search/wrapper-main/wrapper-main.vue
@@ -0,0 +1,30 @@
+<template>
+  <wrapper-template
+    class="wrapper-main"
+    :size="{ width: '50%', height: '100%' }"
+  >
+    <div class="wrapper-main__row">
+      <container-favorites />
+      <container-search />
+    </div>
+  </wrapper-template>
+</template>
+
+<script>
+import { ContainerFavorites } from '../container-favorites';
+import { ContainerSearch } from '../container-search';
+import { WrapperTemplate } from '@/components/common/wrapper-template';
+
+export default {
+  name: 'wrapper-main',
+  components: {
+    ContainerFavorites,
+    ContainerSearch,
+    WrapperTemplate,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'wrapper-main';
+</style>
