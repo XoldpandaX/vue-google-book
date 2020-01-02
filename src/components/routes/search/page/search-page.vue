43ab1b915d473b99b57338e71e14fc0260e912d2
XoldpandaX <rembo-87@yandex.ru>
2020-01-02 10:06:40 +0700

    add: new route forlder && new page component

---
+++ src/components/routes/search/page/search-page.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="search-page">
+    <h1>This is an about page</h1>
+    <button @click="f">sign out</button>
+  </section>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'search-page',
+  methods: {
+    ...mapActions('auth', ['signOut']),
+
+    async f() {
+      await this.signOut();
+      await this.$router.push({ name: ROUTES.HOME.NAME });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'search-page';
+</style>
