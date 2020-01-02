2065bc0957161fd973229349b769ea2c7ad0c54a
XoldpandaX <rembo-87@yandex.ru>
2020-01-02 22:38:15 +0700

    add: new spinner component

---
+++ src/components/common/loader-clip/index.js
@@ -0,0 +1,1 @@
+export { default as LoaderClip } from './loader-clip.vue';
---
+++ src/components/common/loader-clip/loader-clip.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="loader-clip"
+    :style="styles"
+  />
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'loader-clip',
+  props: {
+    isLoading: VueTypes.bool.def(false),
+    color: VueTypes.string.def('#F5A623'),
+    size: VueTypes.string.def('35px'),
+  },
+  computed: {
+    styles() {
+      return {
+        height: this.size,
+        width: this.size,
+        borderColor: `${this.color} ${this.color} transparent`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'loader-clip';
+</style>
