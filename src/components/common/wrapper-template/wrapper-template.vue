56ed5cf556d1a81e54ae518eab83ff3b9bdd66fd
XoldpandaX <rembo-87@yandex.ru>
2020-01-03 01:28:54 +0700

    add: wrapper component to wrap main app blocks

---
+++ src/components/common/wrapper-template/wrapper-template.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :style="styles">
+    <slot />
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'wrapper-template',
+  props: {
+    size: VueTypes.shape({
+      width: VueTypes.string,
+      height: VueTypes.string,
+    }).isRequired,
+    backgroundColor: VueTypes.string.def('#FFF'),
+    backgroundImage: VueTypes.string.def(''),
+  },
+  computed: {
+    styles() {
+      const { size, backgroundImage, ...rest } = this.$props;
+      return {
+        ...rest,
+        width: size.width,
+        height: size.height,
+        backgroundImage: `url(${this.backgroundImage})`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+      };
+    },
+  },
+};
+</script>
