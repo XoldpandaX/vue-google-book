67c8e96926fa5d2333b11b083951a3dcca1ffaaa
XoldpandaX <rembo-87@yandex.ru>
2020-01-02 10:12:19 +0700

    add: add getter for access to user id

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -3,4 +3,5 @@ import isEmpty from 'lodash.isempty';
 export default {
   isUserLoggedIn: ({ user }) => !isEmpty(user),
   isAuthInProcess: ({ isInProcess }) => isInProcess,
+  userId: ({ user: { uid } }) => uid,
 };
