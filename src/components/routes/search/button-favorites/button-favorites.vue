3b378b962a65135ef3b18d302ad252a5c20efb13
XoldpandaX <rembo-87@yandex.ru>
2020-01-03 01:30:31 +0700

    add: new component for handle add to favorite logic

---
+++ src/components/routes/search/button-favorites/button-favorites.vue
@@ -0,0 +1,22 @@
+<template>
+  <button-icon
+    icon-name="heart"
+    :size="{ width: 20, height: 18.35 }"
+    @button-click="$emit('button-favorite-click')"
+  />
+</template>
+
+<script>
+import { ButtonIcon } from '@/components/common/button-icon';
+
+export default {
+  name: 'button-favorites',
+  components: {
+    ButtonIcon,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'button-favorites';
+</style>
---
+++ src/components/routes/search/button-favorites/index.js
@@ -0,0 +1,1 @@
+export { default as ButtonFavorites } from './button-favorites.vue';
