0c72ca2cfebd02a4b5a85f95ad8c1fd0160fa96f
XoldpandaX <rembo-87@yandex.ru>
2020-01-02 10:07:24 +0700

    update: router for get to the app store

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,44 @@ import VueRouter from 'vue-router';
 import { ROUTES } from '@/constants';
 
 import { HomePage } from '@/components/routes/home/page';
-import About from '@/views/About.vue';
+import { SearchPage } from '@/components/routes/search/page';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: ROUTES.HOME.PATH,
-    name: ROUTES.HOME.NAME,
-    component: HomePage,
-  },
-  {
-    path: ROUTES.SEARCH.PATH,
-    name: ROUTES.SEARCH.NAME,
-    component: About,
-  },
-];
+export default (store) => {
+  const router = new VueRouter({
+    mode: 'history',
+    routes: [
+      {
+        path: ROUTES.HOME.PATH,
+        name: ROUTES.HOME.NAME,
+        component: HomePage,
+        meta: {
+          isAuthRequired: false,
+        },
+      },
+      {
+        path: ROUTES.SEARCH.PATH,
+        name: ROUTES.SEARCH.NAME,
+        component: SearchPage,
+        meta: {
+          isAuthRequired: true,
+        },
+      },
+    ],
+  });
 
-const router = new VueRouter({
-  mode: 'history',
-  routes,
-});
+  router.beforeEach((to, from, next) => {
+    if (to.meta.isAuthRequired && !store.getters['auth/isUserLoggedIn']) {
+      next({ name: ROUTES.HOME.NAME });
+    }
 
-export default router;
+    if (!to.meta.isAuthRequired && store.getters['auth/isUserLoggedIn']) {
+      next({ name: ROUTES.SEARCH.NAME });
+    }
+
+    next(true);
+  });
+
+  return router;
+};
