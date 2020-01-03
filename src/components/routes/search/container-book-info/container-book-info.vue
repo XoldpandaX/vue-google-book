f5de996aaa01ba093a42cbe148f2ca9996041091
XoldpandaX <rembo-87@yandex.ru>
2020-01-03 19:27:24 +0700

    add: logic conrainer to inject book info

--- src/components/routes/search/container-book-info/container-book-info.vue
+++ src/components/routes/search/container-book-info/container-book-info.vue
@@ -1,14 +1,19 @@
 <template>
-  <book-info />
+  <book-info :book="chosenBook" />
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import { BookInfo } from '../book-info';
 
 export default {
   name: 'container-book-info',
   components: {
     BookInfo,
+  },
+  computed: {
+    ...mapGetters('library', ['chosenBook']),
   },
 };
 </script>
