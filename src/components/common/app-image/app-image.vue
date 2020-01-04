a5a875bd72763278d965ae6dda0468a5e422d72b
XoldpandaX <rembo-87@yandex.ru>
2020-01-04 14:41:09 +0700

    add: new component for image rendering

---
+++ src/components/common/app-image/app-image.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-image">
+    <img
+      :width="size.width"
+      :height="size.height"
+      :alt="alt"
+      :src="src"
+    >
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types';
+
+export default {
+  name: 'app-image',
+  props: {
+    src: VueTypes.string.isRequired,
+    alt: VueTypes.string.isRequired,
+    size: VueTypes.shape({
+      width: VueTypes.number,
+      height: VueTypes.number,
+    }).def(() => ({ width: 177, height: 242 })),
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'app-image';
+</style>
