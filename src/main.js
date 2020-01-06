49459bb5061319d755511e51f829a74c450a7228
XoldpandaX <rembo-87@yandex.ru>
2020-01-06 13:40:54 +0700

    add: logic to fetch all favorite books from user library on page reload

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ firebase.auth().onAuthStateChanged(async (user) => {
   // received user and doesn't have user data in the store - (in case of page reloading)
   if (!app && user) {
     await store.dispatch('auth/saveUser', user);
+    await store.dispatch('library/fetchAllUserFavoriteBooks');
   }
 
   if (!app) {
