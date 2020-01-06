77033dbe4cc4d03949fb731c14293405d6779d27
XoldpandaX <rembo-87@yandex.ru>
2020-01-06 13:40:06 +0700

    add: new global consts wich are influencing on all library entities

--- src/constants/library.js
+++ src/constants/library.js
@@ -1,8 +1,10 @@
 /**
- * All these objects are associated with the library storage
- * module and the components that consume them
+ * All these values are associated with the library storage
+ * module and the components that consumes them
   */
 
 export default {
   BOOKS_PER_PAGE: 4,
+  FAVORITES_VIEW_MODE: 'FAVORITES_VIEW',
+  SEARCH_VIEW_MODE: 'SEARCH_VIEW',
 };
